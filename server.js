6e5cd2ec683c39446bd64f953656015b31582598
Peter Wildberger <pete.wildberger@gmail.com>
2017-12-11 16:12:34 -0600

    express

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express'),
+  path = require('path'),
+  app = express(),
+  bodyParser = require('body-parser');
+
+const port = process.env.PORT || 3004;
+
+app.use(express.static('client/build'));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res) => {
+  console.log('base url hit');
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, function() {
+  console.log('up 3004');
+});
